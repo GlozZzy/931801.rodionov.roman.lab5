be31a76f57f7c9ee2c72c53c9127970ce217f943
Roman Rodionov <kpackl14@gmail.com>
2021-06-04 17:10:45 +0700

    Добавьте файлы проекта.

---
+++ lab2_5/Models/Hospitals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab2_5.Models
+{
+    public class HospitalModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Display(Name = "Phones")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"8+9+[0-9]{9}", ErrorMessage = "Invalid data")]
+        public string Phones { get; set; }
+
+    }
+}
---
+++ lab2_5/Models/Patients.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab2_5.Models
+{
+    public class PatientModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Diagnosis is required")]
+        [Display(Name = "Diagnosis")]
+        public string Diagnosis { get; set; }
+
+        [Required(ErrorMessage = "Diagnosis is required")]
+        [Display(Name = "Age")]
+        public string Age { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Display(Name = "Phones")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"8+9+[0-9]{9}", ErrorMessage = "Invalid data")]
+        public string Phones { get; set; }
+    }
+}
